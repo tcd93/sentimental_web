2f29f3dff87f42482d1aea65b3e731f492a7d447
tcd93 <dungtang93@gmail.com>
2025-04-08 10:08:14 +0700

    Update config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -11,8 +11,3 @@ export const CACHE_TTL_EMPTY_SECONDS = 15 * 60;
 // Athena Polling Configuration
 export const ATHENA_POLL_INTERVAL_MS = 1000; // Milliseconds between checking query status
 export const ATHENA_MAX_POLL_ATTEMPTS = 10;  // Max number of times to check status
-
-// Basic validation to warn if the essential S3 path isn't configured properly
-if (!process.env.ATHENA_OUTPUT_LOCATION && ATHENA_OUTPUT_LOCATION.includes("your-bucket-name")) {
-    console.warn("WARN: ATHENA_OUTPUT_LOCATION environment variable is not set and the default value is being used. Please configure it.");
-} 
